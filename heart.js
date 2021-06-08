8196af78c68b3a78db589421a110e630166569b1
trim-tab <37784788+trim-tab@users.noreply.github.com>
2021-06-07 18:37:18 -0700

    changes

--- heart.js
+++ heart.js
@@ -1,14 +1,44 @@
+var count = 4;
 function toggleHeart() {
+
+// adds animate class when clicked and updates counter
 window.onclick = e => {
 	var heart = e.target;
-	heart.classList.add('animate');
-  
+	var test = document.getElementById("1");
+	var x = document.createElement("SPAN");
+	var t = document.createTextNode(count);
+	test.appendChild(x);
+	x.appendChild(t);
+	
+	x.classList.add('what');
+	x.classList.add('up');
+	count++;
+  let counter = heart.nextElementSibling;
+	
+	
+	document.body.appendChild(x);
+	
 //   adds class with icon swap
    setTimeout(() => {
         heart.classList.toggle('active');
-   }, 150);
-    
-   setTimeout(() => heart.classList.remove('animate'), 300);
+				counter.classList.toggle('active');
+
+				var t = document.createTextNode(count);
+				heart.removeChild(t);	
+				
+			
+		}, 150);
+    // removes animate class and updates counter
+   setTimeout(() => {
+		//  heart.classList.remove('animate');
+		 	// update counter
+		 count--;
+		}, 300);
 		}
+	
 }   	
 
+
+
+
+	
