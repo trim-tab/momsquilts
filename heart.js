b2ae692da6b3d14847824e30e90fc450b2e9b785
trim-tab <37784788+trim-tab@users.noreply.github.com>
2021-06-14 13:01:56 -0700

    current working

--- heart.js
+++ heart.js
@@ -1,44 +1,85 @@
-var count = 4;
-function toggleHeart() {
+var count1 = "1";
+var count2 = "2";
 
+// when you click adds 2 counters
+function toggleHeart(e) {
+		
+		var heart = e.target;
+		let counter = heart.nextElementSibling;
+		var firstCount = document.createTextNode(count1);
+		var secondCount = document.createTextNode(count2);
+		
+	//   adds class with icon swap
+		setTimeout(() => {
+					heart.classList.toggle('active');
+					counter.classList.toggle('active');
+					counter.nextElementSibling.classList.toggle('active');
+
+					if(counter.className === "counter" && counter.className !== "counter active"){
+						console.log("working");
+						
+						// console.log(counter.firstChild);
+						// counter.removeChild(firstCount);
+					
+					}
+					if(counter.nextElementSibling.className ==="counter2" && counter.nextElementSibling.className !== "counter2 active"){
+						counter.nextElementSibling.removeChild(secondCount);
+						// count2++;
+						console.log("remove2");
+						
+					}
+					console.log(counter.firstChild); 
+					if(counter.className == "counter active"){
+						counter.appendChild(firstCount);
+						console.log(firstCount);	
+						
+					}
+
+					if(counter.nextElementSibling.className == "counter2 active"){
+						counter.nextElementSibling.appendChild(secondCount);
+						
+					}
+				
+			}, 150);
+			// removes animate class and updates counter
+		setTimeout(() => {
+			 heart.classList.remove('animate');
+				// heart.nextElementSibling.removeChild(firstCount);
+				
+			// console.log("countONE " + count1);
+			// console.log("countTWO " + count2);
+			
+
+			}, 300);   	
+
+}
+
+const button = document.querySelector("span");
+	button.addEventListener("click", toggleHeart);
 // adds animate class when clicked and updates counter
-window.onclick = e => {
-	var heart = e.target;
-	var test = document.getElementById("1");
-	var x = document.createElement("SPAN");
-	var t = document.createTextNode(count);
-	test.appendChild(x);
-	x.appendChild(t);
-	
-	x.classList.add('what');
-	x.classList.add('up');
-	count++;
-  let counter = heart.nextElementSibling;
-	
-	
-	document.body.appendChild(x);
-	
-//   adds class with icon swap
-   setTimeout(() => {
-        heart.classList.toggle('active');
-				counter.classList.toggle('active');
+// window.onclick = e => {
 
-				var t = document.createTextNode(count);
-				heart.removeChild(t);	
+
+// var heart = e.target;
+// 	let counter = heart.nextElementSibling;
+// 	var t = document.createTextNode(count);
+// 	counter.appendChild(t);
+// 	count++;
+// //   adds class with icon swap
+//    setTimeout(() => {
+//         heart.classList.toggle('active');
+// 				counter.classList.toggle('active');
+
+// 			// heart.removeChild(t);	
 				
 			
-		}, 150);
-    // removes animate class and updates counter
-   setTimeout(() => {
-		//  heart.classList.remove('animate');
-		 	// update counter
-		 count--;
-		}, 300);
-		}
+// 		}, 150);
+//     // removes animate class and updates counter
+//    setTimeout(() => {
+// 		//  heart.classList.remove('animate');
+// 		 	// update counter
+// 		 count--;
+// 		}, 300);
+// 		}
 	
-}   	
-
-
-
-
-	
+// }   	
